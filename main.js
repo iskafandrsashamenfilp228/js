f817a3bab32b5f67778ee5b460a57d1ede3862d3
Toretocode <aleksandr.filippov0209@gmail.com>
2025-04-13 11:25:23 +0300

    рр
    ррр

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-let voin = prompt("Как твоё имя странник?")
-alert("Ступай и победи дракона!" + voin)
+let calculatorchik = {
+    num0: null,
+    num1: null,
+    read() {
+        this.num0 = +prompt('Напечатай первое число : ', 0)
+        this.num1 = +prompt('Напечатай второе число : ', 0)
+    },
+    sum() {
+        return this.num0 + this.num1
+    },
+    mul() {
+        return this.num0 * this.num1
+    }
+}
+calculatorchik.read()
+console.log(calculatorchik.sum())
+console.log(calculatorchik.mul())
+
